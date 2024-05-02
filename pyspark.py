ffa017b880a1fa526cd9d33804b9776512090c9f
Jirasak Saimek <116647943+jirasaksaimekJijo@users.noreply.github.com>
2024-05-02 10:37:30 +0000

    jo

---
+++ pyspark.py
@@ -0,0 +1,28 @@
+import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("MyPySparkApp").getOrCreate()
+
+#read file csv
+df = spark.read.csv("username.csv", header=True, inferSchema=True)
+
+#read file json
+df = spark.read.json("zipcodes.json", inferSchema=True)
+
+#read find parquet
+df = spark.read.parquet("MT cars.parquet", inferSchema=True)
+
+from pyspark.sql.functions import col
+
+df = spark.range(10).repartition(5)  # Repartition into 5 partitions
+df = df.repartition(col("id"))  # Partition by the "id" column
+
+df = spark.range(10).repartition(5).coalesce(2)  # Reduce to 2 partitions
+
+df = spark.createDataFrame([("A", 1), ("B", 2), ("A", 3)], ["category", "value"])
+df = df.partitionBy("category") 
+
+stations.write.format("delta").saveAsTable("default.table_name")
+
+data.writeTo("mdm.table_name")\
+          .tableProperty("location", "s3://path/to/location/") \
+          .create()
